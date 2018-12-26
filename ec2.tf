db8e4f71cc33d58f112949b5c9b1d5539e65cff7
Prashanth Pullaikodi <pullaikodi@mb149.local>
2018-12-26 13:20:45 +0100

    initial_commit

---
+++ ec2.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
+
+
+resource "aws_instance" "web" {
+  ami = "${lookup(var.ami, var.region)}"
+  instance_type = "t2.micro"
+  key_name = "ppnair-1"
+  security_groups = [
+   "My-SG"
+ ]
+
+ provisioner "remote-exec" {
+  inline = [
+    "sudo yum install -y httpd",
+    "sudo echo 'Welcome to my web page' > /var/www/html/index.html",
+    "sudo /bin/systemctl restart httpd.service",
+    ]
+  }
+
+  connection {
+      type     = "ssh"
+      user="${var.INSTANCE_USERNAME}"
+      private_key="${file("${var.PATH_TO_PRIVATE_KEY}")}"
+      #private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+ tags {
+  name = "My-web"
+   }
+
+}
+
+resource "aws_security_group" "My-SG" {
+name = "My-SG"
+description = "security group that allows ssh and all egress traffic"
+
+  egress {
+     from_port = 80
+     to_port = 80
+     protocol = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+     from_port = 443
+     to_port = 443
+     protocol = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+ ingress {
+   from_port = 22
+   to_port = 22
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+output "public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "public_dns" {
+  value = "${aws_instance.web.public_dns}"
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+
+variable "access_key" {
+   default = "paste_your_access_key_here"
+}
+
+variable "secret_key" {
+  default = "paste_your_secret_key_here"
+}
+
+variable "region" {
+  default = "ap-south-1" #Provide you region here
+}
+
+
+variable "ami" {
+  Âtype = "map"
+  default = {
+    ap-south-1 = "ami-5b673c34" #Provide your ami ID here
+    }
+}
+
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "paste_your_private_key_path_here"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "paste_your_public_key_path_here"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "paste_your_EC2_User_name_here"
+}
